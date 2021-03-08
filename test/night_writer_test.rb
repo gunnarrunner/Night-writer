364d0ff3d96173f1618a126ed967f31b2cb9f5b6
Gunnar Runkle <dunkcrunk@gmail.com>
2021-03-08 15:19:25 -0700

    Iteration 1

---
+++ test/night_writer_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/night_writer'
+
+class NightWriterTest < Minitest::Test
+  def test_it_exists
+    night_writer = NightWriter.new("message.txt", "braille.txt")
+    assert_instance_of NightWriter, night_writer
+  end
+
+  def test_it_can_get_a_braille_letter
+    night_writer = NightWriter.new("message.txt", "braille.txt")
+    input = "a"
+    expected = ["0.", "..", ".."]
+    puts expected
+    assert_equal expected, night_writer.find_braille(input)
+  end
+
+  def test_it_can_get_two_braille_letters
+    skip
+    night_writer = NightWriter.new("message.txt", "braille.txt")
+    input = "a"
+    expected = ["0.", "..", ".."]
+    puts expected
+    assert_equal expected, night_writer.find_braille(input)
+  end
+end
